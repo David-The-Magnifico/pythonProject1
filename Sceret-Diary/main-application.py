ac350bb81af1a5ab40743555368d831dd68cd87e
David <www.wealthydavid@gmail.com>
2024-02-27 12:54:16 +0100

    commit on FEB_24, 2024

--- Sceret-Diary/main-application.py
+++ Sceret-Diary/main-application.py
@@ -53,3 +53,8 @@ username_label.pack()
 username_entry = tk.Entry(root)
 username_entry.pack()
 
+password_label = tk.Label(root, text="Password:")
+password_label.pack()
+password_entry = tk.Entry(root, show="*")
+password_entry.pack()
+
