503e562de51c21c4d074d7a2d2749c086cfae65e
David <www.wealthydavid@gmail.com>
2024-02-27 12:25:58 +0100

    commit on FEB_24, 2024

--- Sceret-Diary/entry.py
+++ Sceret-Diary/entry.py
@@ -7,3 +7,17 @@ def __init__(self, title, body):
 def getId(self):
     return self.id
 
+def setId(self, id):
+    self.id = id
+
+def getTitle(self):
+    return self.title
+
+def setTitle(self, title):
+    self.title = title
+
+def getBody(self):
+    return self.body
+
+def setBody(self, body):
+    self.body = body
