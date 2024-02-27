8168d6201ef322f9361f9f2fc9b5d07cdaa8148d
David <www.wealthydavid@gmail.com>
2024-02-27 12:44:16 +0100

    commit on FEB_24, 2024

--- Sceret-Diary/main-application.py
+++ Sceret-Diary/main-application.py
@@ -19,3 +19,6 @@ class Diary:
     def deleteEntry(self, id):
         self.entries = [entry for entry in self.entries if entry.getId() != id]
 
+    def getEntries(self):
+        return self.entries
+
