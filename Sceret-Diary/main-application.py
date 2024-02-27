14bd82b443cc8902c6697f1b41094ef3dac501b2
David <www.wealthydavid@gmail.com>
2024-02-27 12:36:02 +0100

    commit on FEB_24, 2024

--- Sceret-Diary/main-application.py
+++ Sceret-Diary/main-application.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 
+class Diary:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.entries = []
+
