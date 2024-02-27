8f59448728d5a6467652cbf0619e0adfd252a73f
David <www.wealthydavid@gmail.com>
2024-02-27 11:36:00 +0100

    commit on FEB_24, 2024

--- Sceret-Diary/diary.py
+++ Sceret-Diary/diary.py
@@ -2,3 +2,9 @@
 def addEntry(self, entry):
     self.entries.append(entry)
 
+def findEntryById(self, id):
+    for entry in self.entries:
+        if entry.getId() == id:
+            return entry
+    return None
+
