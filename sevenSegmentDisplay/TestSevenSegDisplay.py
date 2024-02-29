a67acdd1eb23cda7b3a36095d8bd4cea19ad0ed5
David <www.wealthydavid@gmail.com>
2024-02-29 08:29:13 +0100

    commit on FEB_29, 2024

--- sevenSegmentDisplay/TestSevenSegDisplay.py
+++ sevenSegmentDisplay/TestSevenSegDisplay.py
@@ -9,7 +9,7 @@ class TestSevenSegDisplay(unittest.TestCase):
         SevenSegDisplay.fillA()
         self.assertEqual(SevenSegDisplay.segment, [[1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
 
- def test_fillB(self):
+    def test_fillB(self):
         SevenSegDisplay.fillB()
         self.assertEqual(SevenSegDisplay.segment, [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]])
 
@@ -20,4 +20,8 @@ class TestSevenSegDisplay(unittest.TestCase):
     def test_fillD(self):
         SevenSegDisplay.fillD()
         self.assertEqual(SevenSegDisplay.segment, [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1]])
+
+    def test_fillE(self):
+        SevenSegDisplay.fillE()
+        self.assertEqual(SevenSegDisplay.segment, [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]])
 
