468f67f2fb51a49e94e4f2fc5e3cec48e63b9e06
David <www.wealthydavid@gmail.com>
2024-03-04 06:40:29 +0100

    commit on MAR_04, 2024

--- TicTacToe/GameBoard.py
+++ TicTacToe/GameBoard.py
@@ -2,3 +2,10 @@ class GameBoard:
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
+    def make_move(self, player, row, col):
+        if not (0 <= row < 3 and 0 <= col < 3):
+            raise InvalidCellNumberException("Invalid cell number")
+        if self.board[row][col] != ' ':
+            raise CellOccupiedException("Cell already occupied")
+        self.board[row][col] = player
+
