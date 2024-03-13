3bb5bfd2774e5f06fcdcbf2586b38b328d4d09c8
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-13 04:06:31 +0100

    Ecommerce Store app

---
+++ E_Store/Items.py
@@ -0,0 +1,8 @@
+from E_Store import Product
+
+
+class Items:
+
+    def __init__(self, product: Product, product_quantity: int):
+        self.product = product
+        self.product_quantity = product_quantity
